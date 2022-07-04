f38a3d68c470c865e696b7b164f859e2019aeb35
Seif Omran <seifomran1324@gmail.com>
2022-07-04 10:17:05 +0200

    Adding Header and meals

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import mealsImage from '../../assets/meals.jpg'
+import HeaderCardBtn from './HeaderCardBtn';
+
+import classes from './Header.module.css'
+
+function Header(props) {
+    return (
+        <React.Fragment>
+            <header className={classes.header}>
+                <h1>React Meals</h1>
+                <HeaderCardBtn></HeaderCardBtn>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImage} alt='Table contains a lot of food'/>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Header;
